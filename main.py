0323d994c6a6181f4f8db63e37aae4af3c86847c
Anthony-Fournier <adavidfour@yahoo.com>
2021-11-26 12:12:49 -0500

    restart

--- main.py
+++
@@ -1,8 +0,0 @@
-# Anthony David Fournier    Project Started 11/22/2021
-# A personal project for an acceptable music playing assistant
-
-def main():
-    
-
-if __name__ == '__main__':
-    main()
---
+++ pmp.py
@@ -0,0 +1,7 @@
+# Anthony David Fournier    Project Started 11/22/2021
+# A personal project for an acceptable music playing assistant
+
+# Local test file /home/tony/Music/Wu-Tang Clan/Enter The Wu-Tang (36 Chambers) [Expanded Edition]/10 - Protect Ya Neck [Explicit].mp3
+# YouTube test link https://www.youtube.com/watch?v=c8c6BTVMP64
+
+
